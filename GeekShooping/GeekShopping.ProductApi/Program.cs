51526a454c61277e84c1d214f7b99f6d14c195c2
rafam <rafam@RAFAEL>
2023-05-29 08:41:56 -0300

    conectionDB and Migration Product

---
+++ GeekShooping/GeekShopping.ProductApi/Program.cs
@@ -0,0 +1,43 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+            builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(
+                connection,
+                new MySqlServerVersion(new Version(8, 0, 29)))
+            );
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
