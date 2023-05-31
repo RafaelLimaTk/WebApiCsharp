1d6cfe0a0d2a61c9c73c3257da154c3b5b572a5e
rafam <rafam@RAFAEL>
2023-05-31 12:49:31 -0300

    creating product screen and returning the product

---
+++ GeekShooping/GeekShopping.web/Controllers/Produto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.web.Controllers
+{
+    public class Produto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
